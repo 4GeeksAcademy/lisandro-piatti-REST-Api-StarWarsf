6966e63a08d36f97fb87db424cbe94feac8b3666
Lisandro Piatti <159830695+Lichatti@users.noreply.github.com>
2024-07-20 14:45:51 +0000

    add endpoints

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,98 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# people1 = People (
+#     name = "Luke Skywalker",
+#     hair_color = "blond",
+#     eye_color = "blue",
+#     skin_color = "fair"   
+# )
+# db.session.add(people1)
+# db.session.commit()
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+# people2 = People(
+#     name = "C-3PO",
+#     hair_color = "n/a", 
+#     eye_color = "gold",
+#     skin_color = "yellow"
+# )
+# db.session.add(people2)
+# db.session.commit()
 
-    return jsonify(response_body), 200
+# people3 = People(
+#     name = "R2-D2",
+#     hair_color = "n/a",
+#     eye_color = "red",
+#     skin_color = "white, blue"
+# )
+# db.session.add(people3)
+# db.session.commit()
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    users_list = list(map(lambda user : user.serialize(), users))
+    return jsonify(users_list), 200
+
+@app.route('/users/<int:user_id>', methods = ['GET'])
+def get_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return jsonify({ "info" : "Not Found"}), 404
+    
+    print(user)
+    return jsonify(user.serialize()), 200
+
+@app.route('/users', methods = ['POST'])
+def create_user():
+    user_body = request.get_json()
+    user_db = User(
+        username = user_body["username"],
+        email = user_body["email"],
+        password = user_body["password"],
+        is_active = user_body["is_active"]
+    )
+    db.session.add(user_db)
+    db.session.commit()
+    return jsonify(user_db.serialize()), 201
+    
+
+
+'''
+People Endpoints
+
+'''
+
+@app.route('/people', methods=['GET'])
+def get_people_list():
+    people_db = People.query.all()
+    people_list = list(map(lambda people : people.serialize(), people_db))
+    return jsonify(people_list), 200
+
+@app.route('/people/<int:people_id>', methods = ['GET'])
+def get_people(people_id):
+    people = People.query.filter_by(id=people_id).first()
+    if people is None:
+        return jsonify({ "info" : "Not Found"}), 404
+    return jsonify(people.serialize()), 200
+
+'''
+Planets Endpoints
+
+'''
+
+@app.route('/planets', methods=['GET'])
+def get_planets_list():
+    planets_db = Planets.query.all()
+    planets_list = list(map(lambda planet : planet.serialize(), planets_db))
+    return jsonify(planets_list), 200
+
+@app.route('/planets/<int:planets_id>', methods = ['GET'])
+def get_planets(planets_id):
+    planet = Planets.query.filter_by(id=planets_id).first()
+    if planet is None:
+        return jsonify({ "info" : "Not Found"}), 404
+    return jsonify(planet.serialize()), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -5,9 +5,9 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -16,7 +16,7 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "username": self.username,
             # do not serialize the password, its a security breach
         }
     
